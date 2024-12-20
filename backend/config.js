2489f0a8fde8cc62fcf2eea35d6deccc31b3bb4e
Varad <varadsyadav16@gmail.com>
2024-12-21 00:30:18 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://2103029:xwLMb0f3z95vWLP0@bookstore.ho9sq.mongodb.net/?retryWrites=true&w=majority&appName=BookStore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
